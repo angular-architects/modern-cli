a9673706959819430718a6bec216b21f9ef85096
Manfred Steyer <manfred.steyer@gmx.net>
2023-09-28 23:26:59 +0200

    add findBy rx method

--- src/app/domains/ticketing/data/ticketing.store.ts
+++ src/app/domains/ticketing/data/ticketing.store.ts
@@ -1,8 +1,11 @@
-import { inject } from '@angular/core';
+import { inject, Signal } from '@angular/core';
+import { toObservable } from '@angular/core/rxjs-interop';
+
 import {
   patchState,
   selectSignal,
   signalStore,
+  withHooks,
   withMethods,
   withSignals,
   withState,
@@ -10,19 +13,21 @@ import {
 import { Flight } from './flight';
 import { FlightService } from './flight.service';
 import { addMinutes } from 'src/app/shared/util-common';
+import { debounceTime, filter, switchMap } from 'rxjs';
 
 export type Criteria = { from?: string; to?: string };
 
 export const TicketingStore = signalStore(
   { providedIn: 'root' },
   withState({
-    from: '',
-    to: '',
+    from: 'Graz',
+    to: 'Hamburg',
     flights: [] as Flight[],
     basket: {} as Record<number, boolean>,
   }),
   withSignals(({ from, to }) => ({
     flightRoute: selectSignal(() => from() + ' to ' + to()),
+    criteria: selectSignal(() => ({ from: from(), to: to() })),
   })),
   withMethods((state) => {
     const flightService = inject(FlightService);
@@ -38,44 +43,41 @@ export const TicketingStore = signalStore(
         );
         patchState(state, { flights });
       },
+      loadBy(criteria: Signal<{ from: string; to: string }>): void {
+        toObservable(criteria)
+          .pipe(
+            filter((c) => c.from.length >= 3 && c.to.length >= 3),
+            debounceTime(300),
+            switchMap((c) => flightService.find(c.from, c.to))
+          )
+          .subscribe((flights) => {
+            patchState(state, { flights });
+          });
+      },
       updateBasket(flightId: number, selected: boolean): void {
-        patchState(state, (state) => ({
-          ...state,
+        patchState(state, ({ basket }) => ({
           basket: {
-            ...state.basket,
+            ...basket,
             [flightId]: selected,
           },
         }));
       },
       delay(): void {
-        patchState(state, (state) => ({
-          ...state,
+        patchState(state, ({ flights }) => ({
           flights: [
             {
-              ...state.flights[0],
-              date: addMinutes(state.flights[0].date, 15),
+              ...flights[0],
+              date: addMinutes(flights[0].date, 15),
             },
-            ...state.flights.slice(1),
+            ...flights.slice(1),
           ],
         }));
       },
     };
+  }),
+  withHooks({
+    onInit: ({ criteria, loadBy }) => {
+      loadBy(criteria);
+    },
   })
 );
-
-// @Injectable({ providedIn: 'root' })
-// export class FlightFacade {
-//   private flightService = inject(FlightService);
-
-//   private state = signalState(initState);
-
-//   readonly from = this.state.from;
-//   readonly to = this.state.to;
-//   readonly flights = this.state.flights;
-//   readonly basket = this.state.basket;
-
-//   readonly flightRoute = selectSignal(
-//     this.from,
-//     this.to,
-//     (from, to) => from + ' to ' + to
-//   );
